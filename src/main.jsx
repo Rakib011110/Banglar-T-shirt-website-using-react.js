611d410e65ca1b2677990b5c6a05b43b3665158a
Rakibul Islam <rakib011110@gmail.com>
2023-04-08 12:13:00 +0600

    json file add

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "./Components/Home/Home";
+import MainLayout from "./Layout/MainLayout";
+import OrderReview from "./Components/OrderReview/OrderReview";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        loader: () => fetch("tshirt.json"),
+      },
+      {
+        path: "/review",
+        element: <OrderReview />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
